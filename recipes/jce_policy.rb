debec43a9b69ea954284f65543f24b39bb176ac5
Aaron Nichols <anichols@tangogroup.com>
2013-04-09 13:13:54 -0600

    Removed exit if using java 7

--- recipes/jce_policy.rb
+++ recipes/jce_policy.rb
@@ -21,10 +21,6 @@ jdk_version = node['java']['jdk_version']
 java_home   = node['java']['java_home']
 source_dir  = "/usr/local/src"
 
-if jdk_version.to_i >= 7
-  Chef::Log.error("Java JDK 7 is not yet supported by the JCE Policy recipe")
-end
-
 bash "extract-jce-policy" do
   cwd "/tmp"
   code <<-EOH
